1903bb0823f9174a7869e461c67115916e2b5776
renandoring <renandoring@gmail.com>
2026-03-01 15:41:08 -0300

    Atualizacao com ArrayList e toString

--- src/ConversorDeMoeda.java
+++ src/ConversorDeMoeda.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+public class ConversorDeMoeda implements ConversorFinal {
+    double rateCambio = 0;
+    double dolar = 0;
 
-public class ConversorDeMoeda {
+    public double getRateCambio() {
+        return rateCambio;
+    }
+
+    public void setRateCambio(double rateCambio) {
+        this.rateCambio = rateCambio;
+    }
+
+    public double getDolar() {
+        return dolar;
+    }
+
+    public void setDolar(double dolar) {
+        this.dolar = dolar;
+    }
+
+    public ConversorDeMoeda(double rateCambio, double dolar) {
+        this.rateCambio = rateCambio;
+        this.dolar = dolar;
+    }
+
+    @Override
+    public double conversorDeDolarParaReal() {
+        double dolarConverted = rateCambio * dolar;
+        System.out.println("Dolar: " + dolarConverted);
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return getRateCambio() + " " + getDolar();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        ConversorDeMoeda conversorUm = new ConversorDeMoeda(15.00, 20.00);
+        ConversorDeMoeda conversorDois = new ConversorDeMoeda(20.00, 20.00);
+        ConversorDeMoeda conversorTres = new ConversorDeMoeda(60.00, 40.00);
+
+        ArrayList<ConversorDeMoeda> conversor =  new ArrayList<>();
+
+        conversor.add(conversorUm);
+        conversor.add(conversorDois);
+        conversor.add(conversorTres);
+
+        for (ConversorDeMoeda conversorDeMoeda : conversor) {
+            double dolarConvertido = conversorDeMoeda.getDolar() * conversorDeMoeda.getRateCambio();
+            System.out.println("VALORES RECEBIDOS " +
+                    "\nValor do câmbio R$: " + conversorDeMoeda.getRateCambio() +
+                    "\nValor do dolar R$: " + conversorDeMoeda.getDolar());
+            System.out.println("Valor convertido: " + dolarConvertido + "\n");
+        }
     }
 }
