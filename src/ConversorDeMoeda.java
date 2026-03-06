b1f1de2055c9d2fc08e86cc4d5afaa23457353d1
renandoring <renandoring@gmail.com>
2026-03-06 19:49:28 -0300

    Add JSON

--- src/ConversorDeMoeda.java
+++ src/ConversorDeMoeda.java
@@ -1,6 +1,24 @@
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ConversorDeMoeda implements ConversorFinal {
-    double rateCambio = 0;
-    double dolar = 0;
+    private double rateCambio = 0;
+    private double dolar = 0;
+    private Double resultados;//NOVA LISTA QUE VAI RECEBER MINHA CONVWERSAO
+
+    public ConversorDeMoeda(double rateCambio, double dolar) {
+        this.rateCambio = rateCambio;
+        this.dolar = dolar;
+        this.resultados = null;
+    }
+
+    public Double getResultados() {
+        return resultados;
+    }
 
     public double getRateCambio() {
         return rateCambio;
@@ -18,20 +36,18 @@ public class ConversorDeMoeda implements ConversorFinal {
         this.dolar = dolar;
     }
 
-    public ConversorDeMoeda(double rateCambio, double dolar) {
-        this.rateCambio = rateCambio;
-        this.dolar = dolar;
-    }
-
     @Override
     public double conversorDeDolarParaReal() {
         double dolarConverted = rateCambio * dolar;
         System.out.println("Dolar: " + dolarConverted);
-        return 0;
+        this.resultados = dolarConverted;
+        return dolarConverted;
     }
 
     @Override
     public String toString() {
-        return getRateCambio() + " " + getDolar();
+        return "Rate Cambio: " + rateCambio +
+                "\nDolar: " + dolar +
+                "\nResultados: " + resultados;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,46 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
+        //CHAMANDO GSON PARA TTO DE UPPERCASE E CRIAR LISTA
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        ArrayList<ConversorDeMoeda> conversor =  new ArrayList<>();
+
         ConversorDeMoeda conversorUm = new ConversorDeMoeda(15.00, 20.00);
         ConversorDeMoeda conversorDois = new ConversorDeMoeda(20.00, 20.00);
         ConversorDeMoeda conversorTres = new ConversorDeMoeda(60.00, 40.00);
-
-        ArrayList<ConversorDeMoeda> conversor =  new ArrayList<>();
 
         conversor.add(conversorUm);
         conversor.add(conversorDois);
         conversor.add(conversorTres);
 
         for (ConversorDeMoeda conversorDeMoeda : conversor) {
-            double dolarConvertido = conversorDeMoeda.getDolar() * conversorDeMoeda.getRateCambio();
+            conversorDeMoeda.conversorDeDolarParaReal();//PREENCHE A LISTA RESULTADOS
+
+            Double resultado = conversorDeMoeda.getResultados();
             System.out.println("VALORES RECEBIDOS " +
                     "\nValor do câmbio R$: " + conversorDeMoeda.getRateCambio() +
                     "\nValor do dolar R$: " + conversorDeMoeda.getDolar());
-            System.out.println("Valor convertido: " + dolarConvertido + "\n");
+            System.out.println("Valor convertido: " + resultado + "\n");
+        }
+
+        try (FileWriter escrita = new FileWriter("conversos.json")) {
+            escrita.write(gson.toJson(conversor));
+            escrita.close();
+            System.out.println("Escrita realizada com sucesso");
+        } catch (IOException e) {
+            System.out.println("Erro ao gerar arquivo" + e.getMessage());
         }
     }
 }
